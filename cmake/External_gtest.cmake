5d6b1787d82fed1b8d6d50563b74a321c7a652a5
Nate Rosenblum <flander@gmail.com>
2015-07-08 11:57:09 -0700

    Target DLL runtime in Windows gtest build

--- cmake/External_gtest.cmake
+++ cmake/External_gtest.cmake
@@ -4,6 +4,7 @@ set(_build "${CMAKE_CURRENT_BINARY_DIR}/gtest")
 ExternalProject_Add(gtest_ext
     SOURCE_DIR ${_source}
     BINARY_DIR ${_build}
+    CMAKE_ARGS -Dgtest_force_shared_crt=ON -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     # googletest doesn't provide an install target
     INSTALL_COMMAND ""
 )
