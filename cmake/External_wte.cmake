4b9fcea236b60c4def3eba7192c83031a5329b9a
Nate Rosenblum <flander@gmail.com>
2015-07-18 12:33:23 -0700

    Use WTE in the periodic reporter
    Allows use of asynchronous IO to reporting sinks, which will allow
    consistent reporting period (previously, the timeout began after the
    reporting method returned).

---
+++ cmake/External_wte.cmake
@@ -0,0 +1,10 @@
+ExternalProject_Add(wte_ext
+    URL https://github.com/flandr/what-the-event/archive/v0.1.0-alpha.1.tar.gz
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/wte
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DBUILD_TESTS=off
+    BUILD_IN_SOURCE 1
+)
+
+ExternalProject_Get_Property(wte_ext install_dir)
+include_directories(${install_dir}/include)
+link_directories(${install_dir}/lib)
